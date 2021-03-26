dde397e224cc21e5854672ca264efefe2cc7dc2f
MariaDan05 <54112953+MariaDan05@users.noreply.github.com>
2021-03-27 02:11:51 +0400

    Update Homework1.java

---
+++ Homework1.java
@@ -0,0 +1,24 @@
+public class Homework1 {
+    public static void main(String[] args) {
+        byte b = 109;
+        System.out.println("b = " + b);
+        short s = -24030;
+        System.out.println("s = " + s);
+        int a = 305020170;
+        System.out.println("a = " + a);
+        long ll = 123498727160977l;
+        System.out.println("ll = " + ll);
+        double dd = 54930.39493;
+        System.out.println("dd = " + dd);
+        float ff = -937.87654f;
+        System.out.println("ff = " + ff);
+        boolean bb = true;
+        System.out.println("bb = " + bb);
+        boolean bb1 = false;
+        System.out.println("bb1 = " + bb1);
+        char ch = '\u00A6';
+        System.out.println("ch = " + ch);
+        ch = '*';
+        System.out.println("ch = " + ch);
+    }
+}
