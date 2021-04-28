5ad069bd830991c4cd561b53cb70b0e40615fa0a
MariaDan05 <54112953+MariaDan05@users.noreply.github.com>
2021-04-29 00:02:54 +0400

    Create Student.java

---
+++ HomeworkLesson8/Student.java
@@ -0,0 +1,55 @@
+public class Student {
+    private String firstName;
+    private String lastName;
+    private int year;
+    private char gender;
+    private double mark;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public double getMark() {
+        return mark;
+    }
+
+    public void setMark(double mark) {
+        this.mark = mark;
+    }
+
+    public void printInfo() {
+        System.out.printf("First Name: %s, ", firstName);
+        System.out.printf("Last Name: %s, ", lastName);
+        System.out.printf("Year: %d, ", year);
+        System.out.printf("Gender: %s, ", gender == 'm' ? "Male" : "Female");
+        System.out.printf("Mark: %.1f\n", mark);
+    }
+}
