b085b4599854810e4d400a81cc82b925b9f415b1
MariaDan05 <54112953+MariaDan05@users.noreply.github.com>
2021-04-29 00:03:44 +0400

    Create StudentConvertor.java

---
+++ HomeworkLesson8/StudentConvertor.java
@@ -0,0 +1,20 @@
+public class StudentConvertor {
+
+    public static Student[] convertor(String[] strings){
+        Student[] students = new Student[strings.length];
+        for(int i = 0; i < students.length; ++i) {
+            int index = strings[i].indexOf(',');
+            int index1 = strings[i].indexOf(',', index + 1);
+            int index2 = strings[i].indexOf(',', index1 + 1);
+            int index3 = strings[i].indexOf(',', index2 + 1);
+            Student student = new Student();
+            student.setFirstName(strings[i].substring(0, index));
+            student.setLastName(strings[i].substring(index + 1, index1));
+            student.setYear(Integer.parseInt(strings[i].substring(index1 + 1, index2)));
+            student.setGender(strings[i].substring(index2 + 1, index3).charAt(0));
+            student.setMark(Double.parseDouble(strings[i].substring(index3 + 1)));
+            students[i] = student;
+        }
+        return students;
+    }
+}
