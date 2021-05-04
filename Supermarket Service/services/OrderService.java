3fe3e3923d22eb8c532ec3248afd8432cefd472d
MariaDan05 <54112953+MariaDan05@users.noreply.github.com>
2021-05-04 18:33:08 +0400

    Update OrderService.java

--- Supermarket Service/services/OrderService.java
+++ Supermarket Service/services/OrderService.java
@@ -10,43 +10,70 @@ public class OrderService {
     public Order createOrder() {
         Scanner s = new Scanner(System.in);
 
+        String[] strings1 = FileService.readLines("project\\\\java\\\\OOP\\chocolates.txt");
+        Chocolate[] chocolates = ChocolateConvertor.chocolateConvertor(strings1);
+        String[] strings2 = FileService.readLines("project\\\\java\\\\OOP\\jellies.txt");
+        Jelly[] jellies = JellyConvertor.jellyConvertor(strings2);
+        String[] strings3 = FileService.readLines("project\\\\java\\\\OOP\\cakes.txt");
+        Cake[] cakes = CakeConvertor.cakeConvertor(strings3);
+        String[] strings4 = FileService.readLines("project\\\\java\\\\OOP\\breads.txt");
+        Bread[] breads = BreadConvertor.breadConvertor(strings4);
+
         System.out.println("Enter number of costumer 1-5:");
         int num = s.nextInt();
         String[] strings = FileService.readLines("project\\java\\OOP\\customers.txt");
         Customer[] customers = CustomerConvertor.customerConvertor(strings);
         Customer customer = customers[num];
 
-        System.out.println("Enter order number:");
+        System.out.println("Enter order number(100-10000):");
         int orderNumber = s.nextInt();
 
-        System.out.println("Enter number of purchased food products");
+        System.out.println("Enter number of food products you want to order:");
         int numberOfPurchasedProducts = s.nextInt();
 
-        System.out.println("Enter the ordered food products:");
         FoodProducts[] foodProducts = new FoodProducts[numberOfPurchasedProducts];
         for (int i = 0; i < foodProducts.length; i++) {
-            System.out.println("Enter the product you purchased(Sweets/Bakery):");
-            String category = s.next();
-            if(category.equals("Sweets")) {
-                System.out.println("Enter category of Sweets(Chocolate/Jelly)");
-                String category1 = s.next();
-                if(category1.equals("Chocolate")) {
-                    ChocolateService chocolateService = new ChocolateService();
-                    foodProducts[i] = chocolateService.createChocolate();
-                } else if(category1.equals("Jelly")){
-                    JellyService jellyService = new JellyService();
-                    foodProducts[i] = jellyService.createJelly();
+            System.out.println("Choose the category you want to order from(Chocolate/Jelly/Cake/Bread)");
+            String ss = s.next();
+            switch (ss) {
+                case "Chocolate": {
+                    System.out.println("All chocolates:");
+                    System.out.println(FileService.read("project\\\\java\\\\OOP\\\\chocolates.txt"));
+                    System.out.println("Enter the number of chocolate you want to order:");
+                    int n = s.nextInt();
+                    foodProducts[i] = chocolates[n];
+                    break;
                 }
-            } else if(category.equals("Bakery")){
-                System.out.println("Enter category of Bakery(Cake/Bread)");
-                String category1 = s.next();
-                if(category1.equals("Cake")){
-                    CakeService cakeService = new CakeService();
-                    foodProducts[i] = cakeService.createCake();
-                } else if(category1.equals("Bread")) {
-                    BreadService breadService = new BreadService();
-                    foodProducts[i] = breadService.createBread();
+                case "Jelly": {
+                    System.out.println("All jellies:");
+                    System.out.println(FileService.read("project\\\\java\\\\OOP\\\\jellies.txt"));
+                    System.out.println("Enter the number of jelly you want to order:");
+                    int n = s.nextInt();
+                    foodProducts[i] = jellies[n];
+                    break;
                 }
+                case "Cake": {
+                    System.out.println("All cakes:");
+                    System.out.println(FileService.read("project\\\\java\\\\OOP\\\\cakes.txt"));
+                    System.out.println("Enter the number of cake you want to order:");
+                    int n = s.nextInt();
+                    foodProducts[i] = cakes[n];
+                    break;
+                }
+                case "Bread": {
+                    System.out.println("All breads:");
+                    System.out.println(FileService.read("project\\\\java\\\\OOP\\\\breads.txt"));
+                    System.out.println("Enter the number of bread you want to order:");
+                    int n = s.nextInt();
+                    foodProducts[i] = breads[n];
+                    break;
+                }
+                case "Exit": {
+                    break;
+                }
+                default:
+                    System.out.println("Invalid category");
+                    break;
             }
         }
 
